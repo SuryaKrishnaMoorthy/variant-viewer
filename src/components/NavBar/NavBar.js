90e17c2d0a7b8cceed24c05b8b3be4a854397d9e
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2019-01-29 18:55:36 -0800

    make navbar as stateless component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 import "./NavBar.css";
-class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar className="NavBar" expand="md">
-          <NavbarBrand className="NavBrand" href="/">
-            Variant Viewer
-          </NavbarBrand>
-          <Nav className="ml-auto NavItem" navbar>
-            <NavItem>
-              <NavLink className="NavLink" href="/Login/">
-                Login
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
-      </div>
-    );
-  }
-}
+
+const NavBar = props => {
+  return (
+    <div>
+      <Navbar className="NavBar" expand="md">
+        <NavbarBrand className="NavBrand" href="/">
+          Variant Viewer
+        </NavbarBrand>
+        <Nav className="ml-auto NavItem" navbar>
+          <NavItem>
+            <NavLink className="NavLink" href="/Login/">
+              Login
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
 
 export default NavBar;
