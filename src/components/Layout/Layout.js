7b6544cb728b81a158054613647ac22381f08338
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2019-01-29 18:56:34 -0800

    make layout compoinent stateless

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./Layout.css";
 import Aux from "../../hoc/_Aux/_Aux";
 import NavBar from "../NavBar/NavBar";
 import VariantExporter from "../../containers/VariantExporter/VariantExporter";
 
-class Layout extends Component {
-  render() {
-    return (
-      <Aux>
-        <NavBar />
-        <VariantExporter />
-      </Aux>
-    );
-  }
-}
+const Layout = props => {
+  return (
+    <Aux>
+      <NavBar />
+      <VariantExporter />
+    </Aux>
+  );
+};
 
 export default Layout;
